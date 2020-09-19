b7e8be8922b1de616a24a3ab675b523a33d45556
arieljr10699 <arieljr10699@gmail.com>
2020-09-19 16:35:31 -0400

    init

---
+++ LaPlaza.js
@@ -0,0 +1,70 @@
+const Alexa = require('ask-sdk');
+let skill;
+
+exports.handler = async function (event, context) {
+    //console.log('REQUEST ' + JSON.stringify(event));
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addErrorHandlers(ErrorHandler)
+            .addRequestHandlers(
+            // delete undefined built-in intent handlers
+            
+            LaunchRequestHandler,
+            LaPlazaHandler
+           
+            ).create();
+    }
+
+    const response = await skill.invoke(event, context);
+    //console.log('RESPONSE :' + JSON.stringify(response));
+    return response;
+};
+const LaPlazaHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'laPlaza';
+    },
+    handle(handlerInput) {
+        // invoke custom logic of the handler
+        const product = String(Alexa.getSlotValue(handlerInput.requestEnvelope, 'product'));
+
+        
+        const speechText = 'This is my custom intent handler';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle(handlerInput) {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log('Error handled: ' + JSON.stringify(error.message));
+        // console.log('Original Request was:', JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+
+        const speechText = 'Sorry, your skill encountered an error';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Bienvenido a La Plaza, ¿en qué puedo ayudarle?';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
